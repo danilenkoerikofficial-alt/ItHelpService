4e974373b0f5168d84db2b113289a4c94fbe0482
Erik <danilenkoerikofficial@gmail.com>
2025-08-24 23:29:09 +0300

    Создал и стилизовал Pagination, настроил анимацию

--- app/js/app.js
+++ app/js/app.js
@@ -2,7 +2,8 @@ import { Swiper } from "swiper";
 import { Parallax } from "swiper/modules";
 import { Mousewheel } from "swiper/modules";
 import { Controller } from "swiper/modules";
-Swiper.use([ Parallax, Mousewheel ])
+import { Pagination } from "swiper/modules";
+Swiper.use([ Parallax, Mousewheel, Pagination ])
 document.addEventListener('DOMContentLoaded', () => {
 
 	const swiperIMG = new Swiper('.slider-img',{
@@ -21,9 +22,37 @@ document.addEventListener('DOMContentLoaded', () => {
 	},
 	controller: {
 		control: swiperIMG,
-	}
+	},
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true
+	},
+	speed: 2000, // медленная скорость перехода
+    effect: 'fade', // плавное затухание
+    fadeEffect: {
+        crossFade: true
+    },
+    on: {
+        transitionStart: function () {
+            // Добавляем класс анимации для всех буллетов
+            const bullets = document.querySelectorAll('.swiper-pagination-bullet');
+            bullets.forEach(bullet => {
+                bullet.style.transition = 'all 1.2s cubic-bezier(0.25, 0.46, 0.45, 0.94)';
+            });
+        },
+        transitionEnd: function () {
+            // Возвращаем стандартную анимацию после перехода
+            setTimeout(() => {
+                const bullets = document.querySelectorAll('.swiper-pagination-bullet');
+                bullets.forEach(bullet => {
+                    bullet.style.transition = '';
+                });
+            }, 1200);
+        }
+    }
+});
 })
 
    swiperIMG.controller.control = swiperText;
 
-})
+
